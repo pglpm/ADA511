7bb8330f147b95561216a919423a5a568085b8a4
pglpm <luca@magnaspesmeretrix.org>
2023-08-24 12:16:32 +0200

    update population inference

--- docs/code/testaccept.R
+++ docs/code/testaccept.R
@@ -1,20 +1,20 @@
-testgain <- function(n=1000, failprob, failcost, passgain, seed=17){
+testaccept <- function(nitems=1000, failprob, failgain, passgain, seed=17){
     ## for tabulated printing
-    spr <- paste0('%',nchar(n),'i')
-    spr2 <- paste0('%',max(nchar(n*failcost), nchar(n*passgain)),'.0f')
-    spr3 <- paste0('%',nchar(signif(failcost*failprob+passgain*(1-failprob),2))+2,'.3f')
+    spr <- paste0('%',nchar(nitems),'i')
+    spr2 <- paste0('%',max(nchar(nitems*failgain), nchar(nitems*passgain)),'.0f')
+    spr3 <- paste0('%',nchar(signif(failgain*failprob+passgain*(1-failprob),2))+2,'.3f')
     ## define and initialize some parameters
     outc <- c('pass','fail')
     gain <- totfail <- totok <- 0
     ## set seed, if given, for reproducibility
     if(!missing(seed)){ set.seed(seed) }
     cat('\n')
-    for(item in 1:n){
+    for(item in 1:nitems){
         ## ## slow it down a little just to make it more exciting
-        ## Sys.sleep(7/(n*4/100000))
+        ## Sys.sleep(7/(nitems*4/100000))
         fail <- sample(c(TRUE, FALSE), 1, prob=c(failprob, 1-failprob))
         if(fail){
-            gain <- gain + failcost
+            gain <- gain + failgain
             totfail <- totfail + 1
         }else{
             gain <- gain + passgain
@@ -29,5 +29,5 @@ testgain <- function(n=1000, failprob, failcost, passgain, seed=17){
             'GAIN/ITEM', sprintf(spr3, signif(gain/item,2))
             )
     }
-    cat('\n== Final gain/item:', signif(gain/n,2),'==\n\n')
+    cat('\n== Final gain/item:', signif(gain/nitems,2),'==\n\n')
 }
