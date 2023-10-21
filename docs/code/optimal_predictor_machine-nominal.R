c60dc343809ab9dda6e59fd5482b403013ba9d88
pglpm <luca@magnaspesmeretrix.org>
2023-10-21 23:44:29 +0200

    update

--- docs/code/optimal_predictor_machine-nominal.R
+++ docs/code/optimal_predictor_machine-nominal.R
@@ -40,18 +40,12 @@ guessmetadata <- function(data, file){
         }
 }
 
-buildK <- function(data, metadata, alphas, verbose=FALSE){
+
+buildK <- function(metadata, data=NULL, alphas=NULL, verbose=FALSE){
 #### Build object encoding background knowledge and learned knowledge
-    ## Sanity checks
-    if(missing(data)){data <- NULL}
-    if(missing(metadata)){metadata <- NULL}
-    if(is.null(data) & is.null(metadata)){stop("either 'data' or 'metadata' argument must be given.")}
     ##
-    if(is.character(data)){
-        data <- fread(data, na.strings='')
-    }
     if(is.character(metadata)){
-        metadata <- fread(metadata, na.strings='')
+        metadata <- fread(metadata, na.strings='', header=TRUE)
     }
     ##
     variates <- metadata[['variate']]
@@ -60,17 +54,21 @@ buildK <- function(data, metadata, alphas, verbose=FALSE){
     names(rgvariates) <- variates
     SS <- prod(rgvariates)
     ##
-    ## Building a discrete mixture of Dirichlet distributions
+    if(is.character(data)){
+        data <- fread(data, na.strings='', header=TRUE)
+    }
+    ##
+    ## Building a Dirichlet-mixture distribution
     ## with concentration params alphas
-    if(missing(alphas) || is.null(alphas)){
+    if(is.null(alphas)){
         alphas <- 2^seq(floor(-log2(SS)), 2, by=1)
     }else if(is.logical(alphas) && alphas){
         alphas <- 1/sqrt(SS)
     }
     ## print(alphas)
     ## freqs encodes the joint frequencies of observed data
-    freqs <- numeric(SS)
-    dim(freqs) <- rgvariates
+    freqs <- numeric(SS) # zero vector
+    dim(freqs) <- rgvariates # zero array
     dimnames(freqs) <- apply(metadata, 1,
                              function(xx){
                                  unname(xx[paste0('V',1:(xx['domainsize']))])
@@ -80,10 +78,19 @@ buildK <- function(data, metadata, alphas, verbose=FALSE){
     if(verbose){cat('\nUpdating internal state of knowledge, please wait... ')}
     ##
     if(!is.null(data)){
+        ## Check consistency of variates in metadata and data
+        if(length(setdiff(variates, colnames(data))) > 0){
+            stop('Some metadata variates missing in data')
+        }
+        if(length(setdiff(colnames(data), variates)) > 0){
+            message('Discarding data variates not present in metadata')
+        }
+        data <- data[,..variates]
+        ##
         ## fill freqs values
         ## (for-loop faster than apply)
         for(arow in 1:nrow(data)){
-            datum <- data[arow, ..variates]
+            datum <- data[arow]
             if(!any(is.na(datum))){
                 temp <- rbind(as.character(datum))
                 freqs[temp] <- freqs[temp] + 1
@@ -93,20 +100,70 @@ buildK <- function(data, metadata, alphas, verbose=FALSE){
     NN <- sum(freqs)
     ##
     ## Calculate probability distribution for the alpha parameters
-    ## frequencies of frequencies for faster iteration
+    ## Use frequencies of frequencies for faster iteration
     ffs <- tabulate(c(freqs)+1)
     iffs <- which(ffs > 0)
     ffs <- ffs[iffs]
-    palphas <- sapply(alphas, function(alpha){
+    valphas <- sapply(alphas, function(alpha){
         sum(ffs * lgamma(iffs-1 + alpha))
-    }) - lgamma(SS*alphas+NN) - SS*lgamma(alphas) + lgamma(SS*alphas)
+    })
     ##
+    ## Updated probabilities of alpha parameters, for frequency forecasts
+    palphas <- valphas - lgamma(SS*alphas+NN) - SS*lgamma(alphas) + lgamma(SS*alphas)
     palphas <- exp(palphas-max(palphas))
     palphas <- palphas/sum(palphas)
     ##
-    if(verbose){cat('Done.\n')}
+    valphas <- valphas - lgamma(SS*alphas+NN+1)
+    valphas <- valphas-max(valphas)
     ##
-    list(freqs=freqs, alphas=alphas, palphas=palphas)
+    list(freqs=freqs, alphas=alphas, valphas=valphas, palphas=palphas)
+}
+
+forecast <- function(K, conditional=NULL){
+#### Calculate conditional or unconditional probability
+    variates <- names(dimnames(K[['freqs']]))
+    SS <- length(K[['freqs']])
+    ## Selection of conditional values
+    ## select subarray of freqs corresponding to the conditional values
+    if(!is.null(conditional)){
+        conditional <- as.list(conditional)
+        ## Check consistency of variates in metadata and conditional
+        if(length(setdiff(variates, names(conditional))) == 0){
+            stop('All variates are in the conditional')
+        }
+        if(length(setdiff(names(conditional), variates)) > 0){
+            message('Discarding conditional variates not present in metadata')
+        }
+        conditional <- conditional[na.omit(match(variates, names(conditional)))]
+        ##
+        iconditional <- match(names(conditional), variates)
+        totake <- as.list(rep(TRUE, length(variates)))
+        totake[iconditional] <- conditional
+        freqs <- do.call(`[`, c(list(K[['freqs']]), totake))
+        if(is.null(dim(freqs))){
+            dim(freqs) <- length(freqs)
+            dimnames(freqs) <- dimnames(K[['freqs']])[-iconditional]
+        }
+    }else{
+        freqs <- K[['freqs']]
+    }
+    ##
+    ## create an array of forecast variates and alphas
+    freqs <- aperm(
+        sapply(K[['alphas']], function(alpha){
+            log(SS*alpha + freqs)
+        }, simplify='array'),
+        c(length(dim(freqs))+1, 1:length(dim(freqs)))
+    )
+    freqs <- freqs - max(freqs) + K[['valphas']]
+    ##
+    temp <- dimnames(freqs)[-1]
+    freqs <- colSums(exp(freqs))
+    if(is.null(dim(freqs))){
+        dim(freqs) <- length(freqs)
+        dimnames(freqs) <- temp
+        }
+    freqs/sum(freqs)
 }
 
 fprobability <- function(K, marginal=NULL, conditional=NULL, Kout=FALSE){
