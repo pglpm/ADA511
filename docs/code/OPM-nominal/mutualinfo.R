bd5ade737698a81558c3fa6c5b9c70deed0194b7
pglpm <luca@magnaspesmeretrix.org>
2023-11-02 08:08:47 +0100

    update

---
+++ docs/code/OPM-nominal/mutualinfo.R
@@ -0,0 +1,23 @@
+mutualinfo <- function(probs, A, B, base=2){
+#### Returns the mutual information between two sets of variates A, B
+    ## load list of variates
+    variates <- names(dimnames(probs))
+    ## Sanity checks of input arguments
+    if(!all(A %in% variates)){
+        message('Discarding A variates not present in probs')
+        A <- A[A %in% variates]
+    }
+    if(!all(B %in% variates)){
+        message('Discarding B variates not present in probs')
+        B <- B[B %in% variates]
+    }
+    if(any(A %in% B)){
+        stop('A and B have variates in common.')
+    }
+    probs <- aperm(probs, c(A,B))/sum(probs)
+    sum(probs * (log2(probs) - log2(outer(
+                                   apply(probs, A, sum),
+                                   apply(probs, B, sum),
+                                   `*`
+                               ))), na.rm=TRUE)/log2(base)
+}
