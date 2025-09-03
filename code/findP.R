5f233155e73a05f6f951ae20a32334ea1070378f
pglpm <pgl@portamana.org>
2025-09-03 15:28:01 +0200

    update prob. inference

--- code/findP.R
+++ code/findP.R
@@ -1,19 +1,24 @@
 findP <- function(x, ...) {
+    ## number of constraints
     nn <- length(substitute(alist(...)))
+    ## find atomic sentences and prepare truth table for DNF
     tvals <- c(FALSE, TRUE)
     atoms <- sort(all.vars(as.formula(substitute(~ alist(x, ...)))))
-    na <- 2L^length(atoms)
     ttable <- list()
     for(i in atoms) {
         ttable[[i]] <- tvals
     }
     combos <- expand.grid(ttable)
-    ## print(combos)
-    ##
+    ## total number of conjunctions
+    na <- 2L^length(atoms)
+    ## print(combos) # for debugging
+
+### Target probability
     Tp <- substitute(x)
     ##
     if(length(Tp[[2]]) == 1 ||
            !(deparse(Tp[[2]][[1]]) == '~')) {
+        ## it doesn't have a conditional
         extraE <- NULL
         Tsupp <- Tp[[2]]
         EqA <- 1L * apply(combos, 1, function(zz){
@@ -24,6 +29,7 @@ findP <- function(x, ...) {
         E[nn + (1:na), ] <- diag(na)
         D <- c(rep('==', nn), rep('>=', na))
     } else {
+        ## it does have a conditional
         extraE <- 0
         Tsupp <- Tp[[2]]
         Tsupp[[1]] <- `&&`
@@ -46,16 +52,17 @@ findP <- function(x, ...) {
         D <- c(rep('==', nn + 1), rep('>=', na + 1))
     }
     F[nn] <- 1
-    ##
-    ##
+
+### Constraint probabilities, analysed one at a time
     Tp <- substitute(alist(...))
+
     for(i in 2:nn) {
         j <- i - 1
         left <- Tp[[i]][[2]]
         right <- Tp[[i]][[3]]
-        ##
+
         if(!is.numeric(try(eval(right), silent = TRUE))) {
-            ## equality between two probabilities
+            ## constraint is equality between two probabilities
             if(length(right) == 2) {
                 temp <- right
                 right <- substitute(a * 1)
@@ -66,7 +73,7 @@ findP <- function(x, ...) {
             }
             coeff <- eval(right[[1]])(1, right[[3]])
             right <- right[[2]]
-            ##
+
             Esuppl <- left[[2]]
             Esuppr <- right[[2]]
             if(
@@ -76,13 +83,14 @@ findP <- function(x, ...) {
                      !(deparse(Esuppr[[1]]) == '~'))
             ) {
                 ## both probabilities have conditional
+                ## we use the rule of cond. prob.
                 if(!(Esuppl[[3]] == Esuppr[[3]])) {
                     stop('invalid conditionals in argument ', i)
                 }
                 Esuppl[[1]] <- `&&`
                 Esuppr[[1]] <- `&&`
             }
-            ##
+
             E[j, ] <- c(
                 1L * apply(combos, 1, function(zz){
                     eval(Esuppl, as.list(zz))
@@ -91,20 +99,20 @@ findP <- function(x, ...) {
                 }),
                 extraE)
         } else {
-            ## right side is numeric
+            ## constraint is numeric
             coeff <- eval(right)
-            ##
+
             Esupp <- left[[2]]
             if(length(Esupp) == 1 ||
                    !(deparse(Esupp[[1]]) == '~')) {
-                ## no conditional
+                ## probability has no conditional
                 E[j, ] <- c(
                     1L * apply(combos, 1, function(zz){
                         eval(Esupp, as.list(zz))
                     }) - coeff,
                     extraE)
             } else {
-                ## conditional
+                ## probability has conditional
                 Esupp[[1]] <- `&&`
                 Econd <- Esupp[[3]]
                 E[j, ] <- c(
@@ -118,9 +126,9 @@ findP <- function(x, ...) {
             }
         }
     }
-    ##
-    ##
-    ## print(list(E = E, F = F, EqA = EqA))
+
+    ## print(list(E = E, F = F, EqA = EqA)) # for debugging
+    ## Find minimum value
     minp <- lpSolve::lp(
         direction = 'min',
         objective.in = EqA,
@@ -128,6 +136,7 @@ findP <- function(x, ...) {
         const.dir = D,
         const.rhs = F
     )
+    ## Find maximum value
     maxp <- lpSolve::lp(
         direction = 'max',
         objective.in = EqA,
@@ -135,5 +144,6 @@ findP <- function(x, ...) {
         const.dir = D,
         const.rhs = F
     )
+
     c(min = minp$objval, max = maxp$objval)
 }
