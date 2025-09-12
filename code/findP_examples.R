098a282987b4f153c5caabfdfaf2ed737e29bc0a
pglpm <pgl@portamana.org>
2025-09-12 07:16:03 +0200

    update datasets

--- code/findP_examples.R
+++ code/findP_examples.R
@@ -135,28 +135,28 @@ findP(
 ## 0.333333 0.333333 
 
 findP(
-    p(c1 ~ y1 & h2 & I),
-    p(c1 | c2 | c3 ~ I) == 1,
-    p(c1 & c2 ~ I) == 0,
-    p(c1 & c3 ~ I) == 0,
-    p(c2 & c3 ~ I) == 0,
-    p(h1 & y1 ~ I) == 0,
-    p(h2 & y2 ~ I) == 0,
-    p(h3 & y3 ~ I) == 0,
-    p(h1 & c1 ~ I) == 0,
-    p(h2 & c2 ~ I) == 0,
-    p(h3 & c3 ~ I) == 0,
-    p(h1 & h2 ~ I) == 0,
-    p(h1 & h3 ~ I) == 0,
-    p(h2 & h3 ~ I) == 0,
-    p(h1 | h2 | h3 ~ I) == 1,
-    p(h2 ~ c1 & y1 & I) == p(h3 ~ c1 & y1 & I),
-    p(c1 ~ I) == 1/3,
-    p(c2 ~ I) == 1/3,
-    p(c3 ~ I) == 1/3,
-    p(c1 ~ y1 & I) == 1/3,
-    p(c2 ~ y1 & I) == 1/3,
-    p(c3 ~ y1 & I) == 1/3
+    P(car1 ~ you1 & host2 & I), # target probability
+    P(car1 | car2 | car3 ~ I) == 1,
+    P(car1 & car2 ~ I) == 0,
+    P(car1 & car3 ~ I) == 0,
+    P(car2 & car3 ~ I) == 0,
+    P(host1 & you1 ~ I) == 0,
+    P(host2 & you2 ~ I) == 0,
+    P(host3 & you3 ~ I) == 0,
+    P(host1 & car1 ~ I) == 0,
+    P(host2 & car2 ~ I) == 0,
+    P(host3 & car3 ~ I) == 0,
+    P(host1 & host2 ~ I) == 0,
+    P(host1 & host3 ~ I) == 0,
+    P(host2 & host3 ~ I) == 0,
+    P(host1 | host2 | host3 ~ I) == 1,
+    P(host2 ~ car1 & you1 & I) == P(host3 ~ car1 & you1 & I),
+    P(car1 ~ I) == 1/3,
+    P(car2 ~ I) == 1/3,
+    P(car3 ~ I) == 1/3,
+    P(car1 ~ you1 & I) == 1/3,
+    P(car2 ~ you1 & I) == 1/3,
+    P(car3 ~ you1 & I) == 1/3
  )
 ##      min      max 
 ## 0.333333 0.333333 
