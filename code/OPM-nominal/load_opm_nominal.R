8694153b2f69288ddf726afea6f68abc78c6e697
pglpm <pgl@portamana.org>
2025-09-21 07:07:14 +0200

    update function location

--- code/OPM-nominal/load_opm_nominal.R
+++
@@ -1,8 +0,0 @@
-source('guessmetadata.R')
-source('buildagent.R')
-source('infer.R')
-source('decide.R')
-source('mutualinfo.R')
-source('rF.R')
-source('plotFsamples1D.R')
-source('tplotfunctions.R')
